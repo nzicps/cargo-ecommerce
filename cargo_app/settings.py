64cff7a81220c808bfce35597a823f49d1e8a7e8
osman <silicasnd@gmail.com>
2025-10-26 11:13:45 +1300

    Fix login and CSRF settings for Render deployment

--- cargo_app/settings.py
+++ cargo_app/settings.py
@@ -69,3 +69,6 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+
+CSRF_TRUSTED_ORIGINS = ['https://cargo-ecommerce.onrender.com']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
