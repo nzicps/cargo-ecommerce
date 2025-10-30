f92d7f6668f01793fbfa54b6ede7d16d3a22039a
osman <silicasnd@gmail.com>
2025-10-31 00:43:12 +1300

    fix: added Render host, static config, and homepage

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -5,3 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
 ]
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse('<h1>Welcome to Cargo E-commerce!</h1>')
+urlpatterns += [path('', home)]
